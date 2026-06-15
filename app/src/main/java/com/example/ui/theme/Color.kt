00194d8f0010fd0b2a7834ff132e611735bf7e7d
amkyawdev <aung.thuyrain.at449@gmail.com>
2026-06-15 14:29:09 +0000

    Update: Implement fully-featured local device file pickers for Video, Sound and Subtitles

---
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.example.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// V Recap Theme Colors
+val VRecapBgStart = Color(0xFF0B132B)
+val VRecapBgEnd = Color(0xFF1C2541)
+val VRecapCardBg = Color(0xFF222B45)
+val VRecapAccentRed = Color(0xFFE53E3E)
+val VRecapAccentRedLight = Color(0xFFFC8181)
+val VRecapTextLight = Color(0xFFF7FAFC)
+val VRecapTextMuted = Color(0xFFA0AEC0)
+val VRecapBorderRed = Color(0xFFE53E3E)
+val VRecapBorderMuted = Color(0xFF4A5568)
+
